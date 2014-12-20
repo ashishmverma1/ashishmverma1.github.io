a94cc4e2085308a6c1c7150c60520615f1cc613d
Ashish Verma <ashishmverma1@gmail.com>
2014-12-20 21:00:18 +0530

    Added preloader
    Added preloader

---
+++ js/preloader.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("#loader-wrapper").css({opacity:"0"});
+	
+	setTimeout(function(){        
+        $("#loader-wrapper").css({display:"none"});
+    }, 1500);
+});
