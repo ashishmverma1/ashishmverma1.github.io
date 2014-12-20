d6d0569a98385136cd1104006fa5ea03c91db6f1
Ashish Verma <ashishmverma1@gmail.com>
2014-12-20 22:16:39 +0530

    Preloader fixed
    Preloader fixed finally after many tests :dancers:

--- js/preloader.js
+++ js/preloader.js
@@ -5,18 +5,3 @@ window.onload = function(){
         $("#loader-wrapper").css({display:"none"});
     }, 1500);
 }
-
-
-/*
-$(document).ready(function() {
-	$("#loader-wrapper").css({opacity:"0"});
-	
-	setTimeout(function(){        
-        $("#loader-wrapper").css({display:"none"});
-    }, 1500);
-});
-function preloadThisStuff()
-{
-	img1 = new Image(1600,900);
-	img1.src = "img/bg.jpg";
-}*/
