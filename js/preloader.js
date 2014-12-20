6315082ac805232ee6ae75ab1feb062c23d907be
Ashish Verma <ashishmverma1@gmail.com>
2014-12-20 21:44:54 +0530

    Preloader test

--- js/preloader.js
+++ js/preloader.js
@@ -1,3 +1,13 @@
+window.onload = function(){
+	$("#loader-wrapper").css({opacity:"0"});
+	
+	setTimeout(function(){        
+        $("#loader-wrapper").css({display:"none"});
+    }, 1500);
+}
+
+
+/*
 $(document).ready(function() {
 	$("#loader-wrapper").css({opacity:"0"});
 	
@@ -5,9 +15,8 @@ $(document).ready(function() {
         $("#loader-wrapper").css({display:"none"});
     }, 1500);
 });
-
 function preloadThisStuff()
 {
 	img1 = new Image(1600,900);
 	img1.src = "img/bg.jpg";
-}
+}*/
