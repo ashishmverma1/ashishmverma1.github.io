6761346d23c33fb755832215060feb937dd9a787
Ashish Verma <ashishmverma1@gmail.com>
2015-01-10 00:27:28 +0530

    More testing

--- mywebsite/js/main.js
+++ mywebsite/js/main.js
@@ -2,4 +2,6 @@ function sendMessage()
 {
 	$("#send-button").css({background:"rgb(51,255,153)"});
 	$("#send-button").css({color:"rgb(75,75,75)"});
+	$("#sendMssgText").css({display:"none"});
+	$("#sentText").css({display:"block"});
 }
