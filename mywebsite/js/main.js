b561936040f8daa718a5b3ffe66d9ae1c273da27
Ashish Verma <ashishmverma1@gmail.com>
2015-01-10 01:39:07 +0530

    More testing

--- mywebsite/js/main.js
+++ mywebsite/js/main.js
@@ -1,3 +1,4 @@
+/*=============== FORM SUBMIT BUTTON =========*/
 function sendMessage()
 {
 	$("#send-button").css({background:"rgb(51,255,153)"});
@@ -5,3 +6,19 @@ function sendMessage()
 	$("#sendMssgText").css({display:"none"});
 	$("#sentText").css({display:"block"});
 }
+
+/*============== SMOOTH SCROLL =============*/
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
