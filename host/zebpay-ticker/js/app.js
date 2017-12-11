5b20bf406d89f3147255ae754570735209a7feb0
Ashish Verma <ashish_verma_1@yahoo.com>
2017-12-12 00:21:14 +0530

    Add speed selector and styling changes

--- host/zebpay-ticker/js/app.js
+++ host/zebpay-ticker/js/app.js
@@ -1,8 +1,8 @@
-angular.module('myApp', ['ngResource']);
+angular.module('zebpayTickerApp', ['ngResource']);
 
 
 /////////////////// SERVER API ///////////////////////
-angular.module('myApp').provider('zebpayData', zebpayDataProvider);
+angular.module('zebpayTickerApp').provider('zebpayData', zebpayDataProvider);
 
 function zebpayDataProvider() {
   let url = 'https://www.zebapi.com/api/v1/market/ticker/btc/inr';
@@ -21,7 +21,7 @@ function zebpayDataProvider() {
 
 
 /////////////////// UI ///////////////////////
-angular.module('myApp').component('ticker', {
+angular.module('zebpayTickerApp').component('ticker', {
   templateUrl: 'js/app.html',
   controller: myController
 });
@@ -31,6 +31,7 @@ function myController($interval, $window, filterFilter, zebpayData) {
   let isFetchInProgress = false;
   vm.$onInit = activate;
   vm.getCurrencyFormattedString = getCurrencyFormattedString;
+  vm.setUpdateSpeed = setUpdateSpeed;
 
   function activate() {
     vm.priceData = {
@@ -46,60 +47,76 @@ function myController($interval, $window, filterFilter, zebpayData) {
       market: 0,
       volume: 0
     };
-    
+
     vm.startTime = Date.now();
-    
+    vm.updateSpeed = 3000;
+
     initializeCharts();
     fetchAndUpdateServerRates();
     startPoller();
   }
-  
+
   function initializeCharts() {
     vm.buyDataPoints = [];
+    vm.sellDataPoints = [];
+
     vm.buyChart = new CanvasJS.Chart("buy-graph", {
     	exportEnabled: true,
     	title :{
-    		text: "Buy Price"
+    		text: "Buy Price (INR/BTC)"
     	},
+      toolTip: {
+        contentFormatter: function(dataSets) {
+          let priceString = '<strong>Rs. ' + getCurrencyFormattedString(dataSets.entries[0].dataPoint.y, false) + '</strong>';
+          let dateString = (new Date((dataSets.entries[0].dataPoint.x * 1000) + vm.startTime)).toLocaleTimeString();
+          return (priceString + ' @ ' + dateString);
+        }
+      },
     	axisY: {
     		includeZero: false
     	},
     	data: [{
-    		type: "spline",
+    		type: "line",
     		markerSize: 0,
     		dataPoints: vm.buyDataPoints
     	}]
     });
-    
-    vm.sellDataPoints = [];
+
     vm.sellChart = new CanvasJS.Chart("sell-graph", {
     	exportEnabled: true,
     	title :{
-    		text: "Sell Price"
+    		text: "Sell Price (INR/BTC)"
     	},
+      toolTip: {
+        contentFormatter: function(dataSets) {
+          let priceString = '<strong>Rs. ' + getCurrencyFormattedString(dataSets.entries[0].dataPoint.y, false) + '</strong>';
+          let dateString = (new Date((dataSets.entries[0].dataPoint.x * 1000) + vm.startTime)).toLocaleTimeString();
+          return (priceString + ' @ ' + dateString);
+        }
+      },
     	axisY: {
     		includeZero: false
     	},
     	data: [{
-    		type: "spline",
+    		type: "line",
     		markerSize: 0,
     		dataPoints: vm.sellDataPoints
     	}]
     });
   }
-  
+
   function getCurrencyFormattedString(value, showSign) {
     if (value === 0) {
       return '0';
     }
-    
+
     let signSymbol = (value > 0) ? '+' : '-';
-    
+
     if (value < 0) {
       value = value * -1;
     }
     value = Math.floor(value);
-    
+
     let formattedString = ('000' + (value % 1000)).substr(-3);
     value = Math.floor(value / 1000);
     while (value > 0) {
@@ -109,10 +126,10 @@ function myController($interval, $window, filterFilter, zebpayData) {
     while (formattedString.length && formattedString[0] === '0') {
       formattedString = formattedString.substr(1);
     }
-    
+
     return (showSign) ? signSymbol + formattedString : formattedString;
   }
-  
+
   function updateServerRates(response) {
     if(response.market != vm.priceData.market ||
       response.buy != vm.priceData.buy ||
@@ -132,14 +149,14 @@ function myController($interval, $window, filterFilter, zebpayData) {
 
       // set window title
       $window.document.title = 'Rs. ' + getCurrencyFormattedString(vm.priceData.market, false);
-      
+
       // update buy price graph
       vm.buyDataPoints.push({
         x: (Date.now() - vm.startTime) / 1000,
         y: vm.priceData.buy
       });
       vm.buyChart.render();
-      
+
       // update sell price graph
       vm.sellDataPoints.push({
         x: (Date.now() - vm.startTime) / 1000,
@@ -148,7 +165,7 @@ function myController($interval, $window, filterFilter, zebpayData) {
       vm.sellChart.render();
     }
   }
-  
+
   function fetchAndUpdateServerRates() {
     if(!isFetchInProgress) {
       isFetchInProgress = true;
@@ -173,9 +190,17 @@ function myController($interval, $window, filterFilter, zebpayData) {
     }
   }
 
+  function setUpdateSpeed(speedInMillis) {
+    vm.updateSpeed = speedInMillis;
+    startPoller();
+  }
+
   function startPoller() {
-    $interval(function() {
+    if (vm.pollerInstancePromise) {
+      $interval.cancel(vm.pollerInstancePromise);
+    }
+    vm.pollerInstancePromise = $interval(function() {
       fetchAndUpdateServerRates();
-    }, 3000);
+    }, vm.updateSpeed);
   }
 }
